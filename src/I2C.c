3ccc91e792513e223d95ab17e284425c92d8db39
ECleverito <eclever12@gmail.com>
2023-03-06 14:37:18 -0700

    Blank Mighty Gecko project for I2C demo for LPEDT

---
+++ src/I2C.c
@@ -0,0 +1,8 @@
+/*
+ * I2C.c
+ *
+ *  Created on: Mar 6, 2023
+ *      Author: erich
+ */
+
+
---
+++ src/I2C.h
@@ -0,0 +1,13 @@
+/*
+ * I2C.h
+ *
+ *  Created on: Mar 6, 2023
+ *      Author: erich
+ */
+
+#ifndef SRC_I2C_H_
+#define SRC_I2C_H_
+
+
+
+#endif /* SRC_I2C_H_ */
